5a8797963dc491ea8a059dfce67b236bec312014
James Dave Navor <james.navor@abedatasolutions.com.ph>
2025-06-24 12:59:36 +0800

    Add basic iOS project structure and files
    - Added `iosAppApp.swift` as the main entry point for the iOS application.
    - Added `ContentView.swift` with a basic SwiftUI layout.
    - Included `Assets.xcassets` with initial `AppIcon` and `AccentColor` configurations.
    - Added `iosApp.entitlements` file.
    - Created the Xcode project file `iosApp.xcodeproj/project.pbxproj` with default configurations for Debug and Release.
    - Included workspace data and user interface state files for Xcode.

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by James Dave Navor on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
